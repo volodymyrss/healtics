fd4dc770d073f07d15e3d9aded07f0a538c99412
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-08-17 14:11:12 +0200

    version and test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='healtics',
-        version='1.1.1',
+        version='1.1.2',
         packages = [
                 "healtics",
             ],
